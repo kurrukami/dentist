a6a021cbf1c01fc9790932fc8d0c1191206ad0b3
kurrukami <zahry.akram@gmail.com>
2021-04-01 14:40:21 +0100

    email prob

--- ze/settings.py
+++ ze/settings.py
@@ -169,7 +169,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = env('CAT_EMAIL')
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = env('CAT_EMAIL_PSWR')
 
